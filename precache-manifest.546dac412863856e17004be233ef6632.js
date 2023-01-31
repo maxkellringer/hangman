ea5ad0cadbe9ce4f761e00f4725e25829d46acb4
Max Kellringer <max.kellringer@gmx.de>
2023-01-31 11:22:34 +0100

    Updates

---
+++ precache-manifest.546dac412863856e17004be233ef6632.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "221f5b6995810f675c18",
+    "url": "/hangman/static/css/main.2f444829.chunk.css"
+  },
+  {
+    "revision": "221f5b6995810f675c18",
+    "url": "/hangman/static/js/main.221f5b69.chunk.js"
+  },
+  {
+    "revision": "1dee63bc1a77cc940b2c",
+    "url": "/hangman/static/js/1.1dee63bc.chunk.js"
+  },
+  {
+    "revision": "e81b4b6d0834211a4833",
+    "url": "/hangman/static/js/runtime~main.e81b4b6d.js"
+  },
+  {
+    "revision": "87f3dbe01f63cb6a3f8983b29e2766ec",
+    "url": "/hangman/index.html"
+  }
+];
